1b0f89e80c104dfaac3c00471f264ac8157c1973
Yury <marchenko.yr@yandex.ru>
2020-05-17 06:34:42 +0300

    Начинаем вёрстку с препроцессором. Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,5 @@
 var navMain = document.querySelector('.main-nav');
 var navBtn = document.querySelector('.main-nav__btn');
-var navBtnClose = document.querySelector('.main-nav__btn--close');
 var navActiveItem = document.querySelector('.site-list__item--active');
 
 
@@ -13,13 +12,6 @@ navBtn.addEventListener('click', function() {
     } else {
         navMain.classList.add('main-nav--closed');
         navMain.classList.remove('main-nav--opened');
-    }
-});
-
-navBtnClose.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--opened')) {
-        navMain.classList.remove('main-nav--opened');
-        navMain.classList.add('main-nav--closed');
     }
 });
 
